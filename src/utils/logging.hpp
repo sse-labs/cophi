8164a9622958728d8ba4152d63355fe9efef224d
Vivek Sarkar <vivek.sarkar@gmail.com>
2024-08-15 10:55:41 -0400

    add more logging

--- src/utils/logging.hpp
+++ src/utils/logging.hpp
@@ -16,8 +16,8 @@ namespace Utils {
 void initializeLogger() {
   try {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    console_sink->set_level(spdlog::level::info);
-    console_sink->set_pattern("[delphi_cpp] [%^%l%$] %v");
+    console_sink->set_level(spdlog::level::trace);
+    console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l]  %v");
 
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/delphi_cpp.log", true);
     file_sink->set_level(spdlog::level::trace);
